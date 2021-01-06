b6b87a2274e2efc32f3d52dc71f39b3b7f85f2b0
Lettuce <chisa.ltc@gmail.com>
2021-01-06 14:41:51 +0900

    add using ref

--- CpLibrary/Program.cs
+++ CpLibrary/Program.cs
@@ -1,4 +1,15 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Globalization;
+using System.Threading;
+using System.Diagnostics;
+
+using static System.Math;
 
 namespace CpLibrary
 {
