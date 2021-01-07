df91c07a06744214dcc1502948b477cbc9747782
Lettuce <chisa.ltc@gmail.com>
2021-01-07 21:09:49 +0900

    add command parser

--- CpProject/Program.cs
+++ CpProject/Program.cs
@@ -8,21 +8,135 @@ using System.Text;
 using System.Globalization;
 using System.Threading;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
+using CommandLine;
 using CpLibrary.Contest;
-
-using static System.Math;
 
 namespace CpLibrary
 {
 	public static class Program
 	{
+		const string workspacePath = @"../../../Workspace/";
+
 		static void Main(string[] args)
 		{
-			SolverBase solver;
-			var sr = new Scanner(new StreamReader("../../../Workspace/A_input.txt"));
-			solver = new SolverA(sr);
-			solver.Run();
-			Console.ReadKey();
+			Directory.SetCurrentDirectory(workspacePath);
+
+			while (true)
+			{
+				using (var cin = new StreamReader(Console.OpenStandardInput()))
+				using (var cout = new StreamWriter(Console.OpenStandardOutput()))
+				{
+					Console.SetIn(cin);
+					Console.SetOut(cout);
+
+					Console.Write(">> ");
+					Console.Out.Flush();
+
+					var regex = new Regex(@"\s\s+");
+					var arg = regex.Replace(Console.ReadLine().Trim(), " ").Split(' ');
+
+					Parser.Default.ParseArguments<RunCommand, DlCommand, SubmitCommand, ExitCommand>(arg)
+						.WithParsed<RunCommand>(opt => Run(opt))
+						.WithParsed<DlCommand>(opt => Download(opt))
+						.WithParsed<SubmitCommand>(opt => Submit(opt))
+						.WithParsed<ExitCommand>(opt => Exit(opt))
+						.WithNotParsed(err => Console.WriteLine("Failed to parse arguments."));
+				}
+			}
 		}
+
+		static void Run(RunCommand opt)
+		{
+			StreamReader sr;
+			StreamWriter sw;
+			var solvers = new Type[]
+			{
+				typeof(SolverA),
+				typeof(SolverB),
+				typeof(SolverC),
+				typeof(SolverD),
+				typeof(SolverE),
+				typeof(SolverF),
+				typeof(SolverG),
+				typeof(SolverH),
+				typeof(SolverI),
+				typeof(SolverJ)
+			};
+			SolverBase solver;
+
+			var program = char.ToUpper(opt.Program) - 'A';
+			if (program < 0) return;
+			if (program >= 10) return;
+
+			sr = new StreamReader(((char)(program + 'A')).ToString() + "_input.txt");
+			sw = new StreamWriter(Console.OpenStandardOutput());
+			if (opt.DumpResult) sw = new StreamWriter(((char)(program + 'A')).ToString() + "_output.txt");
+			if (opt.IsInputConsole) sr = new StreamReader(Console.OpenStandardInput());
+			if (opt.IsSampleTestcases) throw new NotImplementedException();
+			Console.SetIn(sr);
+			Console.SetOut(sw);
+
+			var scanner = new Scanner(sr);
+
+			solver = (SolverBase)Activator.CreateInstance(solvers[program], scanner);
+
+			var startTime = DateTime.Now;
+
+			solver.Run();
+
+			var endTime = DateTime.Now;
+			
+			sr.Close();
+			sw.Close();
+
+			sr = new StreamReader(Console.OpenStandardInput());
+			sw = new StreamWriter(Console.OpenStandardOutput());
+			Console.SetIn(sr);
+			Console.SetOut(sw);
+
+			Console.WriteLine("Program finished.");
+			Console.WriteLine($"Time: {(endTime - startTime).ToString(@"ss\.ffffff")} sec.");
+			Console.Out.Flush();
+		}
+
+		static void Download(DlCommand opt) => throw new NotImplementedException();
+
+		static void Submit(SubmitCommand opt) => throw new NotImplementedException();
+
+		static void Exit(ExitCommand opt) => Environment.Exit(0);
 	}
+
+	[Verb("run", HelpText = "Run the program.")]
+	class RunCommand
+	{
+		[Value(0, Required = true)]
+		public char Program { get; set; }
+
+		[Option('d', "dump", Required = false, HelpText = "Outputs the result on X_output.txt if enabled.")]
+		public bool DumpResult { get; set; }
+
+		[Option('c', "console", Required = false, HelpText = "Uses the standard input.")]
+		public bool IsInputConsole { get; set; }
+
+		[Option('t', "test", Required = false, HelpText = "Run the program on sample testcases.")]
+		public bool IsSampleTestcases { get; set; }
+	}
+
+	[Verb("dl", HelpText = "Downloads the sample testcases.")]
+	class DlCommand
+	{
+		[Value(0, Required = true)]
+		public char Program { get; set; }
+	}
+
+	[Verb("submit", HelpText = "Submits the solution.")]
+	class SubmitCommand
+	{
+		[Value(0, Required = true)]
+		public char Program { get; set; }
+	}
+
+	[Verb("exit", HelpText = "Terminate this program.")]
+	class ExitCommand { }
 }
