fed1ddead74cf28cd1f6f0e8813b02387bc821b6
れたす <chisa.ltc@gmail.com>
2021-12-06 23:38:42 +0900

    Move util methods from Solver to StaticItems

---
+++ CpLibrary/Library/StaticItems/Util.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace CpLibrary
+{
+	public static partial class StaticItems
+	{
+		public static int[] dx4 => new int[] { 1, 0, -1, 0 };
+		public static int[] dy4 => new int[] { 0, 1, 0, -1 };
+		public static int[] dx8 => new int[] { 1, 1, 0, -1, -1, -1, 0, 1 };
+		public static int[] dy8 => new int[] { 0, 1, 1, 1, 0, -1, -1, -1 };
+
+		public static bool YesNo(bool condition)
+		{
+			Console.WriteLine(condition ? "Yes" : "No");
+			return condition;
+		}
+		public static bool YESNO(bool condition)
+		{
+			Console.WriteLine(condition ? "YES" : "NO");
+			return condition;
+		}
+		public static bool yesno(bool condition)
+		{
+			Console.WriteLine(condition ? "yes" : "no");
+			return condition;
+		}
+		public static T SignOutput<T>(int x, T pos, T zero, T neg) => x == 0 ? zero : (x > 0 ? pos : neg);
+
+		public static T[] CreateArray<T>(int n, Func<int, T> func) => Enumerable.Range(0, n).Select(p => func(p)).ToArray();
+		public static T[][] CreateArray<T>(int x, int y, Func<int, int, T> func) => Enumerable.Range(0, x).Select(i => Enumerable.Range(0, y).Select(j => func(i, j)).ToArray()).ToArray();
+		public static T[][][] CreateArray<T>(int x, int y, int z, Func<int, int, int, T> func) => Enumerable.Range(0, x).Select(i => Enumerable.Range(0, y).Select(j => Enumerable.Range(0, z).Select(k => func(i, j, k)).ToArray()).ToArray()).ToArray();
+	}
+}
--- CpLibrary/Solver.cs
+++ CpLibrary/Solver.cs
@@ -21,26 +21,5 @@ namespace CpLibrary
 	{
 		public abstract void Solve();
 		public abstract void Run();
-
-		public bool YesNo(bool condition)
-		{
-			Console.WriteLine(condition ? "Yes" : "No");
-			return condition;
-		}
-		public bool YESNO(bool condition)
-		{
-			Console.WriteLine(condition ? "YES" : "NO");
-			return condition;
-		}
-		public bool yesno(bool condition)
-		{
-			Console.WriteLine(condition ? "yes" : "no");
-			return condition;
-		}
-		public static T SignOutput<T>(int x, T pos, T zero, T neg) => x == 0 ? zero : (x > 0 ? pos : neg);
-
-		public static T[] CreateArray<T>(int n, Func<int, T> func) => Enumerable.Range(0, n).Select(p => func(p)).ToArray();
-		public static T[][] CreateArray<T>(int h, int w, Func<int, int, T> func) => Enumerable.Range(0, h).Select(i => Enumerable.Range(0, w).Select(j => func(i, j)).ToArray()).ToArray();
 	}
-
 }
