03a6df9bbdb1fdf9dd8f1f2e564cb15795e2bba6
れたす <chisa.ltc@gmail.com>
2021-12-06 23:04:50 +0900

    Seperate PopCount Add dxdy

--- CpLibrary/Library/Util/Util.cs
+++ CpLibrary/Library/Util/Util.cs
@@ -9,33 +9,9 @@ namespace CpLibrary
 {
 	public static partial class Util
 	{
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static int PopCount(uint n)
-		{
-			n = (n & 0x55555555) + (n >> 1 & 0x55555555);
-			n = (n & 0x33333333) + (n >> 2 & 0x33333333);
-			n = (n & 0x0f0f0f0f) + (n >> 4 & 0x0f0f0f0f);
-			n = (n & 0x00ff00ff) + (n >> 8 & 0x00ff00ff);
-			n = (n & 0x0000ffff) + (n >> 16 & 0x0000ffff);
-			return (int)n;
-		}
-
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static int PopCount(int n) => PopCount((uint)n);
-
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static int PopCount(ulong n)
-		{
-			n = (n & 0x5555555555555555) + (n >> 1 & 0x5555555555555555);
-			n = (n & 0x3333333333333333) + (n >> 2 & 0x3333333333333333);
-			n = (n & 0x0f0f0f0f0f0f0f0f) + (n >> 4 & 0x0f0f0f0f0f0f0f0f);
-			n = (n & 0x00ff00ff00ff00ff) + (n >> 8 & 0x00ff00ff00ff00ff);
-			n = (n & 0x0000ffff0000ffff) + (n >> 16 & 0x0000ffff0000ffff);
-			n = (n & 0x00000000ffffffff) + (n >> 32 & 0x00000000ffffffff);
-			return (int)n;
-		}
-
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static int PopCount(long n) => PopCount((ulong)n);
+		static int[] dx4 => new int[] { 1, 0, -1, 0 };
+		static int[] dy4 => new int[] { 0, 1, 0, -1 };
+		static int[] dx8 => new int[] { 1, 1, 0, -1, -1, -1, 0, 1 };
+		static int[] dy8 => new int[] { 0, 1, 1, 1, 0, -1, -1, -1 };
 	}
 }
