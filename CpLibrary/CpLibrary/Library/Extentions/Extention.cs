7cd379ab14acbdcb6f8e929de9eb61c743b4cce4
fairy-lettuce <chisa.ltc@gmail.com>
2023-07-26 01:02:38 +0900

    [breaking] change file/directory system to seperate CpLibrary and CpProject solutions

---
+++ CpLibrary/CpLibrary/Library/Extentions/Extention.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpLibrary
+{
+	public static class Extention
+	{
+		public static string Join<T>(this IEnumerable<T> x, string separator = "") => string.Join(separator, x);
+		public static string Join<T>(this IEnumerable<T> x, char separator) => string.Join(separator, x);
+
+		public static int UpperBound<T>(this IList<T> list, T value) => list.BinarySearch(value, true, 0, list.Count, Comparer<T>.Default);
+		public static int LowerBound<T>(this IList<T> list, T value) => list.BinarySearch(value, false, 0, list.Count, Comparer<T>.Default);
+		public static int BinarySearch<T>(this IList<T> list, T value, bool isUpperBound, int index, int length, Comparer<T> comparer)
+		{
+			var ng = index - 1;
+			var ok = index + length;
+			while (ok - ng > 1)
+			{
+				var mid = ng + (ok - ng) / 2;
+				var res = comparer.Compare(list[mid], value);
+				if (res < 0 || (isUpperBound && res == 0)) ng = mid;
+				else ok = mid;
+			}
+			return ok;
+		}
+
+		public static bool Chmax<T>(ref this T a, T b) where T : struct, IComparable<T>
+		{
+			if (a.CompareTo(b) >= 0) return false;
+			a = b;
+			return true;
+		}
+		public static bool Chmin<T>(ref this T a, T b) where T : struct, IComparable<T>
+		{
+			if (a.CompareTo(b) <= 0) return false;
+			a = b;
+			return true;
+		}
+	}
+}
---
+++ CpLibrary/CpLibrary/Solver.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Globalization;
+using System.Threading;
+using System.Diagnostics;
+
+namespace CpLibrary
+{
+	public abstract class SolverBase
+	{
+		public bool StartsOnThread { get; set; } = false;
+		public int Testcases { get; set; } = 1;
+		public Scanner scanner;
+		public StreamWriter writer;
+
+		public abstract void Init();
+		public abstract void Solve();
+
+		public void Run(StreamReader reader, StreamWriter writer)
+		{
+			this.writer = writer;
+			scanner = new Scanner(reader);
+			Console.SetOut(writer);
+			if (StartsOnThread)
+			{
+				var thread = new Thread(new ThreadStart(RunInternal), 1 << 27);
+				thread.Start();
+				thread.Join();
+			}
+			else
+			{
+				RunInternal();
+			}
+		}
+
+		void RunInternal()
+		{
+			Init();
+			var testcases = Testcases;
+			while (testcases-- > 0)
+			{
+				Solve();
+			}
+		}
+	}
+
+	public static partial class OnlineJudge
+	{
+		public static void Run(SolverBase solver)
+		{
+			var sw = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+			var sr = new StreamReader(Console.OpenStandardInput());
+			solver.Run(sr, sw);
+			Console.Out.Flush();
+		}
+	}
+}
