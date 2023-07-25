14982a4bf3839994774faf0f9e02f28c205a3f13
fairy-lettuce <chisa.ltc@gmail.com>
2023-07-25 23:38:27 +0900

    add verify programs

---
+++ CpLibrary.Verify/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Globalization;
+using System.Threading;
+using System.Diagnostics;
+using CpLibrary;
+
+namespace CpLibrary.Verify
+{
+	internal class APlusB : CompetitiveVerifier.ProblemSolver
+	{
+		public override string Url => "https://judge.yosupo.jp/problem/aplusb";
+		public override void Solve()
+		{
+			var sr = new Scanner(new StreamReader(Console.OpenStandardInput()));
+			var a = sr.ReadInt();
+			var b = sr.ReadInt();
+            Console.WriteLine(a + b);
+        }
+	}
+}
