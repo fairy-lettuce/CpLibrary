bf18a08f2babc08312da4852c243822b0f955b53
fairy-lettuce <chisa.ltc@gmail.com>
2023-07-29 06:57:42 +0900

    implement downloader

---
+++ CpLibrary/CpLibrary.Test/Judge/Downloader/DownloaderTest.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CpLibrary.Test.Judge.Downloader
+{
+	public class DownloaderTest
+	{
+		public static IEnumerable<object[]> GetData()
+		{
+			yield return new object[]
+			{
+				"https://atcoder.jp/contests/abc311/tasks/abc311_a",
+				new List<(string, string)>
+				{
+					("5\nACABB\n", "4\n"),
+					("4\nCABC\n", "3\n"),
+					("30\nAABABBBABABBABABCABACAABCBACCA\n", "17\n")
+				}
+			};
+			yield return new object[]
+			{
+				"https://atcoder.jp/contests/arc164/tasks/arc164_a",
+				new List<(string, string)>
+				{
+					("4\n5 3\n17 2\n163 79\n1000000000000000000 1000000000000000000\n", "Yes\nNo\nYes\nYes\n")
+				}
+			};
+		}
+
+		[Theory]
+		[MemberData(nameof(GetData))]
+		public void ABCFetchTest(string url, IEnumerable<(string, string)> testcase)
+		{
+			var dl = new CpLibrary.Judge.Downloader.Downloader().DownloadTestcases(new Uri(url));
+
+			dl.Should().Equal(testcase);
+		}
+	}
+}
