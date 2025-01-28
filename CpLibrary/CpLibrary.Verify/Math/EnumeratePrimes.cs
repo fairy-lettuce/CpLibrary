351a3e1218045a3e8de90b9a6a90ac98dda5e672
fairy-lettuce <chisa.ltc@gmail.com>
2025-01-28 16:35:40 +0900

    verify: EnumeratePrimes

---
+++ CpLibrary/CpLibrary.Verify/Math/EnumeratePrimes.cs
@@ -0,0 +1,31 @@
+﻿using CpLibrary.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpLibrary.Verify.Math
+{
+	// competitive-verifier: document_title EnumeratePrime (Library Checker: Enumerate Primes)
+	internal class EnumeratePrimes : CompetitiveVerifier.ProblemSolver
+	{
+		public override string Url => "https://judge.yosupo.jp/problem/enumerate_primes";
+		public override void Solve()
+		{
+			var sr = new Scanner(new StreamReader(Console.OpenStandardInput()));
+			var (n, a, b) = sr.ReadValue<int, int, int>();
+			var prime = new List<int>();
+			PrimeEnumerator.EnumeratePrime(n, prime);
+
+			var ans = new List<int>();
+			for (int i = 0; i < prime.Count; i++)
+			{
+				if (i >= b && (i - b) % a == 0) ans.Add(prime[i]);
+			}
+
+			Console.WriteLine($"{prime.Count} {ans.Count}");
+			Console.WriteLine(ans.Join(" "));
+		}
+	}
+}
