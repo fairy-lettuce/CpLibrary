cd587184fabafa0dd48593d3b308654aa3aa4693
fairy-lettuce <chisa.ltc@gmail.com>
2025-01-28 15:40:17 +0900

    refactor: seperate files of Set<T> and MultiSet<T>

--- CpLibrary/CpLibrary/Collections/Set.cs
+++ CpLibrary/CpLibrary/Collections/Set.cs
@@ -260,16 +260,6 @@ namespace CpLibrary.Lib
 		}
 	}
 
-	public class MultiSet<T> : Set<T> where T : IComparable<T>
-	{
-		public MultiSet() : base(isMultiSet: true) {}
-		public MultiSet(IComparer<T> comparer) : base(comparer, true) { }
-		public MultiSet(Comparison<T> comparison) : base(comparison, true) { }
-		public MultiSet(IEnumerable<T> list) : base(list, true) { }
-		public MultiSet(IEnumerable<T> list, IComparer<T> comparer) : base(comparer, true) { }
-		public MultiSet(IEnumerable<T> list, Comparison<T> comparison) : base(list, comparison, true) { }
-	}
-
 	public interface IProdSetOperator<T>
 	{
 		public T Identity { get; }
