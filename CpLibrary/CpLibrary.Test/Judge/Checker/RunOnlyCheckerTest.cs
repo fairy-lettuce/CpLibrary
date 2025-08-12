7d7d456ab24a5681965895fee34ea32c4280a9ea
fairy-lettuce <chisa.ltc@gmail.com>
2025-08-12 10:41:42 +0900

    add test for RunOnlyChecker

---
+++ CpLibrary/CpLibrary.Test/Judge/Checker/RunOnlyCheckerTest.cs
@@ -0,0 +1,154 @@
+﻿using CpLibrary.Judge;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CpLibrary.Test.Judge
+{
+	public class RunOnlyCheckerTest
+	{
+		[Theory]
+		[InlineData(3, 1, 4, 1000, JudgeStatus.AC)]
+		[InlineData(1, 5, 6, 1000, JudgeStatus.AC)]
+		public static void AcceptedTest(int a, int b, int sum, int timeLimit, JudgeStatus status)
+		{
+			void Actual(StreamReader reader, StreamWriter writer)
+			{
+				// returns a + b
+				var sr = new Scanner(reader);
+				var (a, b) = sr.ReadValue<int, int>();
+				writer.WriteLine(a + b);
+			}
+
+			var checker = new RunOnlyChecker(Actual);
+			checker.TimeLimit = TimeSpan.FromMilliseconds(timeLimit);
+			var input = $"{a} {b}\n";
+			var output = $"{sum}\n";
+			var result = checker.Run(input, output);
+			result.Status.Should().Be(status);
+		}
+
+		[Theory]
+		[InlineData(3, 1, 4, 1000, JudgeStatus.AC)]
+		[InlineData(1, 5, 6, 1000, JudgeStatus.AC)]
+		public static void WrongAnswerTest(int a, int b, int sum, int timeLimit, JudgeStatus status)
+		{
+			void Actual(StreamReader reader, StreamWriter writer)
+			{
+				// returns a + b, but wrong answer
+				var sr = new Scanner(reader);
+				var (a, b) = sr.ReadValue<int, int>();
+				writer.WriteLine(a + b + 1);
+			}
+
+			var checker = new RunOnlyChecker(Actual);
+			checker.TimeLimit = TimeSpan.FromMilliseconds(timeLimit);
+			var input = $"{a} {b}\n";
+			var output = $"{sum}\n";
+			var result = checker.Run(input, output);
+			result.Status.Should().Be(status);
+		}
+
+		[Theory]
+		[InlineData(3, 1, 4, 500, 600, JudgeStatus.TLE)]
+		public static void TimeLimitExceededTest(int a, int b, int sum, int timeLimit, int waitTime, JudgeStatus status)
+		{
+			void Actual(StreamReader reader, StreamWriter writer)
+			{
+				// returns a + b, but TLE
+				Thread.Sleep(waitTime);
+				var sr = new Scanner(reader);
+				var (a, b) = sr.ReadValue<int, int>();
+				writer.WriteLine(a + b);
+			}
+
+			var checker = new RunOnlyChecker(Actual);
+			checker.TimeLimit = TimeSpan.FromMilliseconds(timeLimit);
+			var input = $"{a} {b}\n";
+			var output = $"{sum}\n";
+			var result = checker.Run(input, output);
+			result.Status.Should().Be(status);
+		}
+
+		[Theory]
+		[InlineData(3, 1, 4, 1000, JudgeStatus.RE)]
+		public static void RuntimeErrorTest(int a, int b, int sum, int timeLimit, JudgeStatus status)
+		{
+			void Actual(StreamReader reader, StreamWriter writer)
+			{
+				// returns a + b, but runtime error occurs
+				throw new Exception("Test");
+			}
+
+			var checker = new RunOnlyChecker(Actual);
+			checker.TimeLimit = TimeSpan.FromMilliseconds(timeLimit);
+			var input = $"{a} {b}\n";
+			var output = $"{sum}\n";
+			var result = checker.Run(input, output);
+			result.Status.Should().Be(status);
+		}
+
+		public static IEnumerable<object[]> GetData()
+		{
+			yield return new object[]
+			{
+				new List<(int a, int b, int sum, JudgeStatus status)>
+				{
+					(3, 1, 4, JudgeStatus.AC),
+					(1, 4, 5, JudgeStatus.AC),
+					(1, 1, 2, JudgeStatus.RE),
+					(10, 10, 100, JudgeStatus.TLE)
+				},
+				new Dictionary<JudgeStatus, int>
+				{
+					{ JudgeStatus.AC, 2 },
+					{ JudgeStatus.RE, 1 },
+					{ JudgeStatus.TLE, 1 }
+				},
+				500
+			};
+		}
+
+		[Theory]
+		[MemberData(nameof(GetData))]
+		public static void MultipleTestcasesTest(
+			IEnumerable<(int a, int b, int sum, JudgeStatus status)> testcases,
+			Dictionary<JudgeStatus, int> expectedDictionary,
+			int timeLimit)
+		{
+			void Actual(StreamReader reader, StreamWriter writer)
+			{
+				// returns a + b, but sometimes wrong...
+				var sr = new Scanner(reader);
+				var (a, b) = sr.ReadValue<int, int>();
+				if (a < b)
+				{
+					writer.WriteLine(a + b + 1);
+					return;
+				}
+				if (a == 1 && b == 1)
+				{
+					throw new Exception("edge case runtime error");
+				}
+				if (a >= 10 && b >= 10)
+				{
+					Thread.Sleep(timeLimit * 2);
+				}
+				writer.WriteLine(a + b);
+			}
+
+			var checker = new RunOnlyChecker(Actual);
+			checker.TimeLimit = TimeSpan.FromMilliseconds(timeLimit);
+			var tc = testcases.Select(p => ($"{p.a} {p.b}\n", $"{p.sum}\n"));
+			var result = checker.Run(tc);
+			result.Status.Should().Be(testcases.Select(p => p.status).Max());
+			result.StatusCount.Should().Equal(expectedDictionary);
+		}
+	}
+}
