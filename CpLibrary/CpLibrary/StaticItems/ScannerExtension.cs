b95d29a7b923dc1f6a18f19cd120000418833a42
fairy-lettuce <chisa.ltc@gmail.com>
2025-08-15 21:25:42 +0900

    [impl] matrix related IO extension methods

---
+++ CpLibrary/CpLibrary/StaticItems/ScannerExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpLibrary;
+
+public static partial class StaticItems
+{
+	public static T[][] ReadMatrix<T>(this Scanner sr, int n, int m)
+		where T : IParsable<T>
+		=> Enumerable.Range(0, n).Select(p => sr.ReadValueArray<T>(m)).ToArray();
+	public static T[][] ReadJaggedArray<T>(this Scanner sr, int n, int[] m)
+		where T : IParsable<T>
+		=> Enumerable.Range(0, n).Select(p => sr.ReadValueArray<T>(m[p])).ToArray();
+}
---
+++ CpLibrary/CpLibrary/StaticItems/WriterExtension.cs
@@ -0,0 +1,17 @@
+﻿using CpLibrary.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpLibrary;
+
+public static partial class StaticItems
+{
+	public static void WriteMatrix<T>(this StreamWriter sw, IEnumerable<IEnumerable<T>> arr) => sw.WriteLine(arr.Select(p => p.Join(" ")).Join("\n"));
+
+	public static void WriteMatrix<T>(this StreamWriter sw, Matrix<T> arr) where T : INumberBase<T> => sw.WriteMatrix((IEnumerable<IEnumerable<T>>)arr);
+}
