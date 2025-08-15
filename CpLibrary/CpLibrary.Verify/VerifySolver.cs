a9b49cf229a445a2e79419125c3bb60c39ed7215
fairy-lettuce <chisa.ltc@gmail.com>
2025-08-15 21:26:11 +0900

    [impl] base class for CompetitiveVerifier.ProblemSolver

---
+++ CpLibrary/CpLibrary.Verify/VerifySolver.cs
@@ -0,0 +1,20 @@
+﻿using CompetitiveVerifier;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpLibrary.Verify;
+
+internal abstract class VerifySolver : ProblemSolver
+{
+	internal Scanner sr;
+	internal StreamWriter sw;
+
+	public VerifySolver() : base()
+	{
+		sr = new Scanner(new StreamReader(Console.OpenStandardInput()));
+		sw = new StreamWriter(Console.OpenStandardOutput());
+	}
+}
