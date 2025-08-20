064256f141e92747aabba3ff75ca08012174408e
fairy-lettuce <chisa.ltc@gmail.com>
2025-08-20 20:42:18 +0900

    [verify] binary search

---
+++ CpLibrary/CpLibrary.Verify/StaticItems/ALDS1_4_B.cs
@@ -0,0 +1,33 @@
+﻿using CpLibrary.Collections;
+using CpLibrary.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using ModInt = AtCoder.StaticModInt<AtCoder.Mod998244353>;
+
+namespace CpLibrary.Verify.Geometry
+{
+	// competitive-verifier: document_title Binary Search (Aizu Online Judge: ALDS1_4_B)
+	internal class BinarySearchTest : VerifySolver
+	{
+		public override string Url => "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_4_B";
+		public override double? Tle => 2.0;
+		public override void Run()
+		{
+			var n = sr.ReadInt();
+			var s = sr.ReadIntArray(n);
+			var q = sr.ReadInt();
+			var t = sr.ReadIntArray(q);
+			var ans = 0;
+			for (int i = 0; i < q; i++)
+			{
+				ans += s.UpperBound(t[i]) - s.LowerBound(t[i]);
+			}
+			sw.WriteLine(ans);
+		}
+	}
+}
