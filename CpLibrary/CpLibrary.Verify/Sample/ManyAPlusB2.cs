ee6cb33a83958b47b0fea91e91eea30a5b76cc9d
fairy-lettuce <chisa.ltc@gmail.com>
2025-08-22 13:40:28 +0900

    add scanner old version for comparison

---
+++ CpLibrary/CpLibrary.Verify/Sample/ManyAPlusB2.cs
@@ -0,0 +1,23 @@
+﻿using CpLibrary;
+
+namespace CpLibrary.Verify
+{
+	// competitive-verifier: document_title IO Benchmark (Library Checker: Many A+B)
+	internal class MnyAPlusBTest2 : CompetitiveVerifier.ProblemSolver
+	{
+		public override string Url => "https://judge.yosupo.jp/problem/many_aplusb";
+		public override double? Tle => 5.0;
+		public override void Solve()
+		{
+			var sr = new ScannerOld(new StreamReader(Console.OpenStandardInput()));
+			var sw = new StreamWriter(Console.OpenStandardOutput());
+
+			var q = sr.ReadInt();
+			for (int i = 0; i < q; i++)
+			{
+				var (x, y) = sr.ReadValue<long, long>();
+				sw.WriteLine(x + y);
+			}
+		}
+	}
+}
