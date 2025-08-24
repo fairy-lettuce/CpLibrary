b19302d00895d950ece054539db97b9b94a2eb54
fairy-lettuce <chisa.ltc@gmail.com>
2025-08-25 03:50:02 +0900

    [verify] ReadString()

---
+++ CpLibrary/CpLibrary.Verify/IO/yukicoder_2298.cs
@@ -0,0 +1,40 @@
+﻿using CpLibrary.Collections;
+using CpLibrary.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using ModInt = AtCoder.StaticModInt<AtCoder.Mod998244353>;
+
+namespace CpLibrary.Verify.IO;
+
+// competitive-verifier: document_title Scaner.ReadString() (yukicoder: No. 2298 yukicounter)
+internal class ReadStringTest : VerifySolver
+{
+	public override string Url => "https://yukicoder.me/problems/no/2298";
+	public override double? Tle => 2.0;
+	public override void Run()
+	{
+		var s = sr.ReadString();
+		var ans = 0;
+		var ren = 0;
+		var tmp = 0;
+		var yukicoder = "yukicoder";
+		for (int i = 0; i < s.Length; i++)
+		{
+			if (s[i] == yukicoder[tmp])
+			{
+				if (++tmp == yukicoder.Length) { tmp = 0; ++ren; ans.Chmax(ren); }
+			}
+			else
+			{
+				tmp = 0;
+				ren = 0;
+			}
+		}
+		sw.WriteLine(ans);
+	}
+}
