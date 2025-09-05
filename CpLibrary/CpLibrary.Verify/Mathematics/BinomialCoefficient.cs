d07a60b2f99fa67d74b1509d8ad91709f58544bd
れたす <chisa.ltc@gmail.com>
2025-09-05 23:28:37 +0900

    [add] binomial coefficient (prime mod)

---
+++ CpLibrary/CpLibrary.Verify/Mathematics/BinomialCoefficient.cs
@@ -0,0 +1,20 @@
+﻿using CpLibrary.Mathematics;
+
+namespace CpLibrary.Verify.Mathematics;
+
+// competitive-verifier: document_title Binomial Coefficient (Library Checker: Binomial Coefficient (Prime Mod))
+internal class BinomialCoefficientTest : VerifySolver
+{
+	public override string Url => "https://judge.yosupo.jp/problem/binomial_coefficient_prime_mod";
+	public override double? Tle => 10.0;
+	public override void Run()
+	{
+		var (t, m) = sr.ReadValue<int, uint>();
+		var f = new BinomialCoefficient(m, 10000000);
+		for (int i = 0; i < t; i++)
+		{
+			var (n, k) = sr.ReadValue<int, int>();
+			sw.WriteLine(f.Binom(n, k));
+		}
+	}
+}
---
+++ CpLibrary/CpLibrary/Mathematics/BinomialCoefficient.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CpLibrary.Mathematics;
+
+public class BinomialCoefficient
+{
+	readonly uint mod;
+	uint[] f, finv, inv;
+
+	public BinomialCoefficient(uint mod, int size)
+	{
+		this.mod = mod;
+		f = new uint[size];
+		finv = new uint[size];
+		inv = new uint[size];
+		Init();
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public uint Binom(int n, int k)
+	{
+		if ((uint)k > (uint)n) return 0;
+		return (uint)(((long)f[n] * finv[k] % mod) * finv[n - k] % mod);
+	}
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public uint Factorial(int n) => f[n];
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public uint Inv(int n) => inv[n];
+
+	void Init()
+	{	
+		f[0] = finv[0] = inv[0] = 1;
+		f[1] = finv[1] = inv[1] = 1;
+		for (int i = 2; i < f.Length; i++)
+		{
+			f[i] = (uint)((long)f[i - 1] * i % mod);
+			inv[i] = (uint)(mod - (long)inv[mod % i] * (mod / i) % mod);
+			finv[i] = (uint)((long)finv[i - 1] * inv[i] % mod);
+		}
+	}
+}
